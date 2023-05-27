f351a5f0f51048ce7c866c6b649c43574ed3e29c
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-05-27 08:06:37 +0300

    testpush

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,11 +1,11 @@
 select_all_female_bears_return_name_and_age = """
 SELECT
-	name,
-	age
+    name,
+    age
 FROM
-	bears
+    bears
 WHERE
-	sex = 'F';
+    sex = 'F';
 """
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
@@ -40,6 +40,7 @@ ORDER BY
     age DESC
 LIMIT 1;
 """
+
 select_youngest_bear_and_returns_name_and_age = """
 SELECT
     name,
@@ -50,3 +51,4 @@ ORDER BY
     age ASC
 LIMIT 1;
 """
+
